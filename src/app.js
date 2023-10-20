c40d588ad3e1ed55ae573b72f5830746ad039241
TheLore85 <garofalo.digital@gmail.com>
2023-10-20 22:03:51 +0200

    project update

--- src/app.js
+++ src/app.js
@@ -5,7 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const sign = ["♦", "♠", "♥", "♣"];
+const color = ["red", "black", "red", "black"];
+const number = document.querySelector("#number");
+const signTop = document.querySelector("#signTop");
+const signBottom = document.querySelector("#signBottom");
+const btn = document.querySelector("#btn");
+
+//random select one number
+function pickNumber() {
+  let number = Math.floor(Math.random() * 11 + 1);
+  return number;
+}
+
+//random select one of the element
+function pickSign() {
+  let i = Math.floor(Math.random() * 4);
+  return i;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
 };
+
+const generateCard = () => {
+  const i = pickSign();
+
+  number.innerHTML = pickNumber();
+  signTop.innerHTML = sign[i];
+  signTop.className = `sign ${color[i]}`;
+
+  signBottom.innerHTML = sign[i];
+  signBottom.className = `sign ${color[i]}`;
+};
+
+/// add events
+btn.addEventListener("click", generateCard);
